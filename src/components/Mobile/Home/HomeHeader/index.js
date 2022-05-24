4aedfd85ea6704bd708d765ae4e3d772db1e475d
Uvaise Mohammed <mohammeduvaisec@gmail.com>
2022-05-24 06:30:16 +0530

    Home Header

---
+++ src/components/Mobile/Home/HomeHeader/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Discount } from "react-iconly";
+
+class HomeHeader extends Component {
+  render() {
+    return (
+      <div className="col-12  sticky-top snaky-home-header">
+        <div className="d-flex align-items-center justify-content-between">
+          <div className="menu-square">
+            <div>
+              <img src="assets/img/Menu.png" alt="menu"></img>
+            </div>
+          </div>
+          <div>
+            <img src="assets/img/snaky-logo-w.png" alt="menu"></img>
+          </div>
+          <div className="menu-square">
+            <Discount set="light" primaryColor="#fe0000" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomeHeader;
