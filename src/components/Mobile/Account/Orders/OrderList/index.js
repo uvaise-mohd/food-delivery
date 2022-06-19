dbd6a94e8739d1d55ecf75ebcbff58eb295e18a3
Mohammed Soofy Waseem <mwsoofy@gmail.com>
2022-06-19 19:30:16 +0530

    orderHistory

--- src/components/Mobile/Account/Orders/OrderList/index.js
+++ src/components/Mobile/Account/Orders/OrderList/index.js
@@ -3,138 +3,337 @@ import React, { Component } from "react";
 import DelayLink from "../../../../helpers/delayLink";
 import Ink from "react-ink";
 import Moment from "react-moment";
-import { TickSquare, ShieldDone, ShieldFail, TimeSquare, Bag2 } from "react-iconly";
+import {
+  TickSquare,
+  ShieldDone,
+  ShieldFail,
+  TimeSquare,
+  Bag2,
+} from "react-iconly";
 import { formatPrice } from "../../../../helpers/formatPrice";
 import OrderCancelPopup from "./OrderCancelPopup";
 import { getSingleItem } from "../../../../../services/items/actions";
 import { connect } from "react-redux";
 import { addProduct } from "../../../../../services/cart/actions";
+import { ChevronDown } from "react-iconly";
 
 class OrderList extends Component {
-	static contextTypes = {
-		router: () => null,
-	};
+  static contextTypes = {
+    router: () => null,
+  };
 
-	componentDidMount() {
-		document.getElementsByTagName("body")[0].classList.add("bg-white");
-	}
+  componentDidMount() {
+    document.getElementsByTagName("body")[0].classList.add("bg-white");
+  }
 
-	_getTotalItemCost = (item) => {
-		let itemCost = parseFloat(item.price) * item.quantity;
-		if (item.order_item_addons.length) {
-			item.order_item_addons.map((addon) => {
-				itemCost += parseFloat(addon.addon_price) * item.quantity;
-				return itemCost;
-			});
-		}
-		return formatPrice(itemCost);
-	};
+  _getTotalItemCost = (item) => {
+    let itemCost = parseFloat(item.price) * item.quantity;
+    if (item.order_item_addons.length) {
+      item.order_item_addons.map((addon) => {
+        itemCost += parseFloat(addon.addon_price) * item.quantity;
+        return itemCost;
+      });
+    }
+    return formatPrice(itemCost);
+  };
 
-	addProducts = (order) => {
-		const { addProduct } = this.props;
-		console.log(order.orderitems);
+  addProducts = (order) => {
+    const { addProduct } = this.props;
+    console.log(order.orderitems);
 
-		order.orderitems.forEach(item => {
-			this.props.getSingleItem(item.item_id).then((response) => {
-				if (response) {
-					response.payload.quantity = 1;
-					addProduct(response.payload);
-				}
-			});
-		});
+    order.orderitems.forEach((item) => {
+      this.props.getSingleItem(item.item_id).then((response) => {
+        if (response) {
+          response.payload.quantity = 1;
+          addProduct(response.payload);
+        }
+      });
+    });
 
-		setTimeout(() => {
-			this.context.router.history.push("/cart");
-		}, 1000);
-	}
+    setTimeout(() => {
+      this.context.router.history.push("/cart");
+    }, 1000);
+  };
 
-	componentWillUnmount() {
-		document.getElementsByTagName("body")[0].classList.remove("bg-grey");
-	}
+  componentWillUnmount() {
+    document.getElementsByTagName("body")[0].classList.remove("bg-grey");
+  }
 
-	render() {
-		const { order, user, cancelOrder } = this.props;
-		return (
-			<React.Fragment>
-				<div className="mb-4">
-					<div className="p-3 order-block">
-						<DelayLink
-							to={`/view-order/${order.unique_order_id}`}
-							// delay={250}
-							style={{ position: "relative" }}
-						>
-							<div>
-								{/* <div className="mb-2" style={{ fontWeight: 'bolder', fontSize: '15px', color: localStorage.getItem("storeColor") }}>#{order.unique_order_id}</div> */}
-								<div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-									<div style={{ fontWeight: 'bolder', fontSize: '15px' }}>{order.stores.name}</div>
-									{order.order_status_id === 1 &&
-										<div style={{ color: '#0960BD', display: 'flex', alignItems: 'center' }}><span><TickSquare className="mr-1" size={14} /></span><span>Order Placed</span></div>
-									}
-									{order.order_status_id === 5 &&
-										<div style={{ color: '#1ABE30', display: 'flex', alignItems: 'center' }}><span><ShieldDone className="mr-1" size={14} /></span><span>Delivered</span></div>
-									}
-									{order.order_status_id === 6 &&
-										<div style={{ color: '#FF0000', display: 'flex', alignItems: 'center' }}><span><ShieldFail className="mr-1" size={14} /></span><span>Cancelled</span></div>
-									}
-									{order.order_status_id === 9 &&
-										<div style={{ color: '#FF0000', display: 'flex', alignItems: 'center' }}><span><ShieldFail className="mr-1" size={14} /></span><span>Transaction Failed</span></div>
-									}
-									{order.order_status_id === 10 &&
-										<div style={{ color: '#8B8B8B', display: 'flex', alignItems: 'center' }}><span><TickSquare className="mr-1" size={14} /></span><span>Transaction Pending</span></div>
-									}
-									{order.order_status_id === 11 &&
-										<div style={{ color: '#FE0B15', display: 'flex', alignItems: 'center' }}><span><Bag2 className="mr-1" size={14} /></span><span>Self Pick Up</span></div>
-									}
-									{order.order_status_id != 1 && order.order_status_id != 5 && order.order_status_id != 6 && order.order_status_id != 9 && order.order_status_id != 10 && order.order_status_id != 11 &&
-										<div style={{ color: '#0960BD', display: 'flex', alignItems: 'center' }}><span><TimeSquare className="mr-1" size={14} /></span><span>On Going</span></div>
-									}
-								</div>
-								<div className="text-muted mt-2">{order.stores.address}</div>
-								<div className="text-muted mt-2 font-w600"><span className="rupees-symbol">₹</span>{order.total}</div>
+  render() {
+    const { order, user, cancelOrder } = this.props;
+    return (
+      <React.Fragment>
+        <div className='mb-4'>
+          <div
+            className='p-3'
+            style={{
+              background: "#FBFBFB",
+              border: "1px solid #F5F5F8",
+              borderRadius: "16px",
+            }}>
+            <div className={`d-flex `}>
+              {order.orderitems.slice(0, 4).map((item) => (
+                <div style={{ marginLeft: "10px" }}>
+                  <img
+                    style={{
+                      width: "4rem",
+                      height: "4rem",
+                      background: "rgba(227, 85, 63, 0.15)",
+                      borderRadius: "10px",
+                    }}
+                    src={`https://app.snakyz.com/assets/img/items/${item.item.image}`}
+                  />
+                </div>
+              ))}
+              {order.orderitems.length > 4 && (
+                <div style={{ marginLeft: "10px" }}>
+                  <div
+                    style={{
+                      width: "4rem",
+                      height: "4rem",
+                      background: "rgba(227, 85, 63, 0.15)",
+                      borderRadius: "10px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      fontWeight: "700",
+                      fontSize: "17px",
+                      color: "#FF0000",
+                    }}>
+                    + {parseFloat(order.orderitems.length) - 4}
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className='mt-4 d-flex justify-content-between'>
+              <div
+                style={{
+                  fontWeight: "400",
+                  color: order.orderstatus.color_code,
+                }}>
+                <span
+                  style={{
+                    background: order.orderstatus.color_code,
+                    borderRadius: "3px",
+                    color: "white",
+                    padding: "0px 1px",
+                  }}>
+                  <ChevronDown
+                    size={"small"}
+                    style={{ marginBottom: "-3px" }}
+                  />
+                </span>{" "}
+                {order.orderstatus.name}
+              </div>
+              <div>
+                #
+                {order.unique_order_id.substring(
+                  order.unique_order_id.length - 8
+                )}
+              </div>
+              <div>
+                <Moment format='MMM DD, hh:mm A'>{order.created_at}</Moment>
+              </div>
+            </div>
 
-								<hr style={{ borderTop: '1px dashed grey' }} />
-								{order.orderitems.map((item) => (
-									<div className="mb-2">
-										<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-											<div>{item.name}</div>
-											<div className="font-w600">x {item.quantity}</div>
-										</div>
-										{item.order_item_addons.map((addon) => (
-											<div className="text-muted ml-4">{addon.addon_name}</div>
-										))}
-									</div>
-								))}
-								<div className="text-muted" style={{ fontSize: '11px' }}><Moment format="MMM DD, hh:mm A">{order.created_at}</Moment></div>
-							</div>
-						</DelayLink>
+            <div className='mb-2 mt-4 d-flex justify-content-between'>
+              <div
+                style={{
+                  background: "rgb(255, 0, 0)",
+                  borderRadius: "10px",
+                  fontSize: "15px",
+                  fontWeight: "400",
+                  color: "white",
+                  width: "45%",
+                  height: "50px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>
+                Re-Order
+              </div>
+              <div
+                style={{
+                  background: "#ecb8aa4a",
+                  borderRadius: "10px",
+                  fontSize: "15px",
+                  fontWeight: "400",
+                  color: "#FF0000",
+                  width: "45%",
+                  height: "50px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}>
+                Details
+              </div>
+            </div>
+            {/* <DelayLink
+              to={`/view-order/${order.unique_order_id}`}
+              // delay={250}
+              style={{ position: "relative" }}>
+              <div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                  }}>
+                  <div style={{ fontWeight: "bolder", fontSize: "15px" }}>
+                    {order.stores.name}
+                  </div>
+                  {order.order_status_id === 1 && (
+                    <div
+                      style={{
+                        color: "#0960BD",
+                        display: "flex",
+                        alignItems: "center",
+                      }}>
+                      <span>
+                        <TickSquare className='mr-1' size={14} />
+                      </span>
+                      <span>Order Placed</span>
+                    </div>
+                  )}
+                  {order.order_status_id === 5 && (
+                    <div
+                      style={{
+                        color: "#1ABE30",
+                        display: "flex",
+                        alignItems: "center",
+                      }}>
+                      <span>
+                        <ShieldDone className='mr-1' size={14} />
+                      </span>
+                      <span>Delivered</span>
+                    </div>
+                  )}
+                  {order.order_status_id === 6 && (
+                    <div
+                      style={{
+                        color: "#FF0000",
+                        display: "flex",
+                        alignItems: "center",
+                      }}>
+                      <span>
+                        <ShieldFail className='mr-1' size={14} />
+                      </span>
+                      <span>Cancelled</span>
+                    </div>
+                  )}
+                  {order.order_status_id === 9 && (
+                    <div
+                      style={{
+                        color: "#FF0000",
+                        display: "flex",
+                        alignItems: "center",
+                      }}>
+                      <span>
+                        <ShieldFail className='mr-1' size={14} />
+                      </span>
+                      <span>Transaction Failed</span>
+                    </div>
+                  )}
+                  {order.order_status_id === 10 && (
+                    <div
+                      style={{
+                        color: "#8B8B8B",
+                        display: "flex",
+                        alignItems: "center",
+                      }}>
+                      <span>
+                        <TickSquare className='mr-1' size={14} />
+                      </span>
+                      <span>Transaction Pending</span>
+                    </div>
+                  )}
+                  {order.order_status_id === 11 && (
+                    <div
+                      style={{
+                        color: "#FE0B15",
+                        display: "flex",
+                        alignItems: "center",
+                      }}>
+                      <span>
+                        <Bag2 className='mr-1' size={14} />
+                      </span>
+                      <span>Self Pick Up</span>
+                    </div>
+                  )}
+                  {order.order_status_id != 1 &&
+                    order.order_status_id != 5 &&
+                    order.order_status_id != 6 &&
+                    order.order_status_id != 9 &&
+                    order.order_status_id != 10 &&
+                    order.order_status_id != 11 && (
+                      <div
+                        style={{
+                          color: "#0960BD",
+                          display: "flex",
+                          alignItems: "center",
+                        }}>
+                        <span>
+                          <TimeSquare className='mr-1' size={14} />
+                        </span>
+                        <span>On Going</span>
+                      </div>
+                    )}
+                </div>
+                <div className='text-muted mt-2'>{order.stores.address}</div>
+                <div className='text-muted mt-2 font-w600'>
+                  <span className='rupees-symbol'>₹</span>
+                  {order.total}
+                </div>
 
-						<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'end' }}>
-							{/* {(order.order_status_id == 5 || order.order_status_id == 6 || order.order_status_id == 9) &&
-								<div
-									className="btn btn-square btn-outline-secondary mb-10 pl-2 pr-2 order-track2-button"
-									style={{ position: "relative", zIndex: "999" }}
-									onClick={() => this.addProducts(order)}
-								>
-									Reorder
-								</div>
-							} */}
-							{order.can_cancel && order.order_status_id == 1 &&
-								<OrderCancelPopup order={order} user={user} cancelOrder={cancelOrder} />
-							}
-						</div>
-					</div>
-				</div>
-			</React.Fragment >
-		);
-	}
+                <hr style={{ borderTop: "1px dashed grey" }} />
+                {order.orderitems.map((item) => (
+                  <div className='mb-2'>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-between",
+                      }}>
+                      <div>{item.name}</div>
+                      <div className='font-w600'>x {item.quantity}</div>
+                    </div>
+                    {item.order_item_addons.map((addon) => (
+                      <div className='text-muted ml-4'>{addon.addon_name}</div>
+                    ))}
+                  </div>
+                ))}
+                <div className='text-muted' style={{ fontSize: "11px" }}>
+                  <Moment format='MMM DD, hh:mm A'>{order.created_at}</Moment>
+                </div>
+              </div>
+            </DelayLink> */}
+
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "end",
+              }}>
+              {order.can_cancel && order.order_status_id == 1 && (
+                <OrderCancelPopup
+                  order={order}
+                  user={user}
+                  cancelOrder={cancelOrder}
+                />
+              )}
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-	user: state.user.user,
-	cancel: state.orders.cancel,
+  user: state.user.user,
+  cancel: state.orders.cancel,
 });
 
-export default connect(
-	mapStateToProps,
-	{ getSingleItem, addProduct }
-)(OrderList);
+export default connect(mapStateToProps, { getSingleItem, addProduct })(
+  OrderList
+);
--- src/components/Mobile/Account/Orders/index.js
+++ src/components/Mobile/Account/Orders/index.js
@@ -6,116 +6,250 @@ import ContentLoader from "react-content-loader";
 import OrderList from "./OrderList";
 import { Redirect } from "react-router";
 import { connect } from "react-redux";
-import { ArrowLeft } from "react-iconly";
+import { ChevronLeft } from "react-iconly";
 
 class Orders extends Component {
-	state = {
-		no_orders: false,
-		cancelSuccess: false,
-	};
+  state = {
+    no_orders: false,
+    cancelSuccess: false,
+    active_tab: "HISTORY",
+  };
 
-	static contextTypes = {
-		router: () => null,
-	};
+  static contextTypes = {
+    router: () => null,
+  };
 
-	componentDidMount() {
-		const { user } = this.props;
-		if (user.success) {
-			this.props.getOrders(user.data.auth_token, user.data.id);
-		}
-	}
+  componentDidMount() {
+    const { user } = this.props;
+    if (user.success) {
+      this.props.getOrders(
+        user.data.auth_token,
+        user.data.id,
+        this.state.active_tab
+      );
+    }
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (nextProps.orders.length === 0) {
-			this.setState({ no_orders: true });
-		}
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.orders.length === 0) {
+      this.setState({ no_orders: true });
+    }
 
-		if (this.props.cancel !== nextProps.cancel) {
-			//call to handle afterCancellation from parent
+    if (this.props.cancel !== nextProps.cancel) {
+      //call to handle afterCancellation from parent
 
-			if (nextProps.cancel.success) {
-				this.setState({ cancelSuccess: true });
-				const { user } = this.props;
-				if (user.success) {
-					this.props.getOrders(user.data.auth_token, user.data.id);
-				}
-			}
-		}
-	}
+      if (nextProps.cancel.success) {
+        this.setState({ cancelSuccess: true });
+        const { user } = this.props;
+        if (user.success) {
+          this.props.getOrders(user.data.auth_token, user.data.id);
+        }
+      }
+    }
 
-	render() {
-		if (window.innerWidth > 768) {
-			return <Redirect to="/" />;
-		}
-		const { user, orders } = this.props;
-		if (localStorage.getItem("storeColor") === null) {
-			return <Redirect to={"/"} />;
-		}
-		if (!user.success) {
-			return <Redirect to={"/login"} />;
-		}
-		return (
-			<React.Fragment>
-				<div className="bg-white">
-					<div style={{ "position": "absolute", "top": "15px", "left": "15px" }} onClick={() => this.context.router.history.push("/my-account")}>
-						<ArrowLeft />
-					</div>
-					<div className="text-center pt-15" style={{ "ontSize": "15px", "fontWeight": "bolder" }}>
-						My Orders
-					</div>
-					<div className="pt-20 pb-20 height-100-percent px-15">
-						{orders.length === 0 && !this.state.no_orders && (
-							<ContentLoader
-								height={600}
-								width={400}
-								speed={1.2}
-								primaryColor="#f3f3f3"
-								secondaryColor="#ecebeb"
-							>
-								<rect x="0" y="0" rx="0" ry="0" width="75" height="22" />
-								<rect x="0" y="30" rx="0" ry="0" width="350" height="18" />
-								<rect x="0" y="60" rx="0" ry="0" width="300" height="18" />
-								<rect x="0" y="90" rx="0" ry="0" width="100" height="18" />
+    this.setState({ loading: false });
+  }
 
-								<rect x="0" y={0 + 170} rx="0" ry="0" width="75" height="22" />
-								<rect x="0" y={30 + 170} rx="0" ry="0" width="350" height="18" />
-								<rect x="0" y={60 + 170} rx="0" ry="0" width="300" height="18" />
-								<rect x="0" y={90 + 170} rx="0" ry="0" width="100" height="18" />
+  render() {
+    if (window.innerWidth > 768) {
+      return <Redirect to='/' />;
+    }
+    const { user, orders } = this.props;
+    if (localStorage.getItem("storeColor") === null) {
+      return <Redirect to={"/"} />;
+    }
+    if (!user.success) {
+      return <Redirect to={"/login"} />;
+    }
 
-								<rect x="0" y={0 + 340} rx="0" ry="0" width="75" height="22" />
-								<rect x="0" y={30 + 340} rx="0" ry="0" width="350" height="18" />
-								<rect x="0" y={60 + 340} rx="0" ry="0" width="300" height="18" />
-								<rect x="0" y={90 + 340} rx="0" ry="0" width="100" height="18" />
-							</ContentLoader>
-						)}
-						{orders.length === 0 && (
-							<div className="text-center mt-50 text-muted">
-								No Orders
-							</div>
-						)}
-						{orders.map((order) => (
-							<OrderList
-								key={order.id}
-								order={order}
-								user={user}
-								cancelOrder={this.props.cancelOrder}
-								cancel={this.props.cancel}
-							/>
-						))}
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+    const activeTab = {
+      background: "rgb(255, 0, 0)",
+      borderRadius: "10px",
+      fontSize: "15px",
+      fontWeight: "600",
+      color: "white",
+      width: "45%",
+      height: "50px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    };
+    const inactiveTab = {
+      background: "#ecb8aa4a",
+      borderRadius: "10px",
+      fontSize: "15px",
+      fontWeight: "600",
+      color: "#FF0000",
+      width: "45%",
+      height: "50px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    };
+
+    return (
+      <React.Fragment>
+        <div className='bg-white'>
+          {/* <div
+            style={{ position: "absolute", top: "15px", left: "15px" }}
+            onClick={() => this.context.router.history.push("/my-account")}>
+            <ArrowLeft />
+          </div> */}
+
+          <div
+            className='d-flex align-items-center justify-content-center p-2'
+            style={{
+              border: "1px solid #BBBDC1",
+              borderRadius: "8px",
+              minHeight: "40px",
+              minWidth: "40px",
+              position: "absolute",
+              top: "15px",
+              left: "15px",
+            }}
+            onClick={() => this.context.router.history.push("/my-account")}>
+            <ChevronLeft primaryColor='#BBBDC1' />
+          </div>
+          <div
+            className='text-center pt-15'
+            style={{
+              fontStyle: "normal",
+              fontWeight: "700",
+              fontSize: "16px",
+              lineHeight: "40px",
+              textAlign: "center",
+              letterSpacing: "2px",
+              textTransform: "uppercase",
+              color: "#111A2C",
+            }}>
+            My Orders
+          </div>
+
+          <div className='mb-4 mt-4 p-2 d-flex justify-content-around'>
+            <div
+              onClick={() => {
+                this.setState({ active_tab: "HISTORY", loading: true });
+                if (user.success) {
+                  this.props.getOrders(
+                    user.data.auth_token,
+                    user.data.id,
+                    "HISTORY"
+                  );
+                }
+              }}
+              style={
+                this.state.active_tab == "HISTORY" ? activeTab : inactiveTab
+              }>
+              History
+            </div>
+            <div
+              onClick={() => {
+                this.setState({ active_tab: "UPCOMING", loading: true });
+                if (user.success) {
+                  this.props.getOrders(
+                    user.data.auth_token,
+                    user.data.id,
+                    "UPCOMING"
+                  );
+                }
+              }}
+              style={
+                this.state.active_tab == "UPCOMING" ? activeTab : inactiveTab
+              }>
+              Upcoming
+            </div>
+          </div>
+
+          <div className='pt-20 pb-20 height-100-percent px-15'>
+            {((orders.length === 0 && !this.state.no_orders) ||
+              this.state.loading == true) && (
+              <ContentLoader
+                height={600}
+                width={400}
+                speed={1.2}
+                primaryColor='#f3f3f3'
+                secondaryColor='#ecebeb'>
+                <rect x='0' y='0' rx='0' ry='0' width='75' height='22' />
+                <rect x='0' y='30' rx='0' ry='0' width='350' height='18' />
+                <rect x='0' y='60' rx='0' ry='0' width='300' height='18' />
+                <rect x='0' y='90' rx='0' ry='0' width='100' height='18' />
+
+                <rect x='0' y={0 + 170} rx='0' ry='0' width='75' height='22' />
+                <rect
+                  x='0'
+                  y={30 + 170}
+                  rx='0'
+                  ry='0'
+                  width='350'
+                  height='18'
+                />
+                <rect
+                  x='0'
+                  y={60 + 170}
+                  rx='0'
+                  ry='0'
+                  width='300'
+                  height='18'
+                />
+                <rect
+                  x='0'
+                  y={90 + 170}
+                  rx='0'
+                  ry='0'
+                  width='100'
+                  height='18'
+                />
+
+                <rect x='0' y={0 + 340} rx='0' ry='0' width='75' height='22' />
+                <rect
+                  x='0'
+                  y={30 + 340}
+                  rx='0'
+                  ry='0'
+                  width='350'
+                  height='18'
+                />
+                <rect
+                  x='0'
+                  y={60 + 340}
+                  rx='0'
+                  ry='0'
+                  width='300'
+                  height='18'
+                />
+                <rect
+                  x='0'
+                  y={90 + 340}
+                  rx='0'
+                  ry='0'
+                  width='100'
+                  height='18'
+                />
+              </ContentLoader>
+            )}
+            {orders.length === 0 && (
+              <div className='text-center mt-50 text-muted'>No Orders</div>
+            )}
+            {orders.map((order) => (
+              <OrderList
+                key={order.id}
+                order={order}
+                user={user}
+                cancelOrder={this.props.cancelOrder}
+                cancel={this.props.cancel}
+              />
+            ))}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-	user: state.user.user,
-	orders: state.orders.orders,
-	cancel: state.orders.cancel,
+  user: state.user.user,
+  orders: state.orders.orders,
+  cancel: state.orders.cancel,
 });
 
-export default connect(
-	mapStateToProps,
-	{ getOrders, cancelOrder }
-)(Orders);
+export default connect(mapStateToProps, { getOrders, cancelOrder })(Orders);
