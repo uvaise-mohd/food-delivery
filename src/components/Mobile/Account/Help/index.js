974f7aad62a17d68a5672ca34877fad5e9600fb6
Uvaise Mohammed <mohammeduvaisec@gmail.com>
2022-07-04 12:17:11 +0530

    help mail changed

--- src/components/Mobile/Account/Help/index.js
+++ src/components/Mobile/Account/Help/index.js
@@ -28,13 +28,13 @@ class Help extends Component {
                         <p className="mb-1 font-weight-bold">Email us on</p>
                         <p>
                             Need help with the Chopze app? Write us an email and we'll get back to you at the earliest.
-                            <p className="mt-2"><a className="font-weight-bold" href="mailto:help@app.snakyz.com" style={{ color: localStorage.getItem("storeColor") }}>help@app.snakyz.com</a></p>
+                            <p className="mt-2"><a className="font-weight-bold" href="mailto:help@app.snakyz.com" style={{ color: localStorage.getItem("storeColor") }}>help@snakyz.com</a></p>
                         </p>
                         <hr />
                         <p className="mb-1 font-weight-bold">Connect us on</p>
                         <p>
                             You can get assistance for any order related queries by calling the number which is
-                            <p className="mt-2"><a className="font-weight-bold" href="tel:+916238298358" style={{ color: localStorage.getItem("storeColor") }}>+91 6238298358</a></p>
+                            <p className="mt-2"><a className="font-weight-bold" href="tel:+971 50 569 4964" style={{ color: localStorage.getItem("storeColor") }}>+971 50 569 4964</a></p>
                         </p>
                     </div>
                 </div>
